1b0729364c1a4d0aa24fb8a1fb86f6f82ffd96cb
mikebavon okello <bavomike@gmail.com>
2022-09-29 09:24:44 +0300

    Implementing servlet using javax.servlet.Servlet interface

--- src/main/java/com/cohort10/example/Hello.java
+++ src/main/java/com/cohort10/example/Hello.java
@@ -6,27 +6,31 @@ import java.io.PrintWriter;
 
 public class Hello implements Servlet {
 
+    ServletConfig config = null;
+
     public void init(ServletConfig config) throws ServletException {
+        System.out.println("***********Hello world is initialized.. place into service*************8");
+        this.config = config;
 
     }
 
     public ServletConfig getServletConfig() {
-        return null;
+        return config;
     }
 
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("The servlet Name in web.xml: " + getServletConfig().getServletName());
         res.setContentType("text/html");
 
-        PrintWriter wr = res.getWriter();
-        wr.print("<html>");
-        wr.print("<head>");
-        wr.print("<title>Example</title>");
-        wr.print("</head>");
-        wr.print("<body>");
-        wr.print("<p>The best team......leaders of EPL - And everyone must join..</p>");
-        wr.print("</body>");
-        wr.print("</html>");
+        String action = req.getParameter("action");
 
+        PrintWriter wr = res.getWriter();
+        if (action != null && action.equalsIgnoreCase("register"))
+            wr.print(this.register());
+        else if (action != null && action.equalsIgnoreCase("login"))
+            wr.print(this.login());
+        else
+            wr.print(this.home());
     }
 
     public String getServletInfo() {
@@ -34,6 +38,54 @@ public class Hello implements Servlet {
     }
 
     public void destroy() {
+        System.out.println("***********Destroying hello world servlet*************");
+    }
 
+    public String home(){
+        return "<!DOCTYPE html>"
+            + "<html> "
+                + "<head> "
+                    + "<h1> Welcome to School Management system</h1>"
+                + "</head>"
+                + "<body>"
+                    + " To Register <a href='./welcome?action=register'>Register</a><br/>"
+                    + " To Loging <a href='./welcome?action=login'>Login</a><br/>"
+                + "</body>"
+            + "</html>";
+    }
+
+    public String register(){
+        return "<!DOCTYPE html>"
+            + "<html> "
+                + "<head> "
+                    + "<h1> Registration</h1>"
+                + "</head>"
+                + "<body>"
+                    + "<form>"
+                        + "<table> "
+                            + "<tr> <td> Email: </td> <td> <input type=”text” email=”email”> </td> </tr> "
+                            + "<tr> <td> Password: </td> <td> <input type=”Password” name=”password”> </td> </tr> "
+                            + "<tr> <td> Confirm Password: </td> <td> <input type=”Password” name=”confirmPassword”> </td> </tr> "
+                        + "</table>"
+                    + "</form>"
+                + "</body>"
+            + "</html>";
+    }
+
+    public String login(){
+        return "<!DOCTYPE html>"
+            + "<html> "
+                + "<head> "
+                    + "<h1> Login</h1>"
+                + "</head>"
+                + "<body>"
+                    + "<form>"
+                        + "<table> "
+                            + "<tr> <td> Email/Username: </td> <td> <input type=”text” email=”username”> </td> </tr> "
+                            + "<tr> <td> Password: </td> <td> <input type=”Password” name=”password”> </td> </tr> "
+                        + "</table>"
+                    + "</form>"
+                + "</body>"
+            + "</html>";
     }
 }
