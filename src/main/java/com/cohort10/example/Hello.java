3a7a2e9d0379c699a99a3d7c336b5f48f0a5f4ba
mikebavon okello <bavomike@gmail.com>
2022-09-29 10:29:10 +0300

    -http servelet implementation

--- src/main/java/com/cohort10/example/Hello.java
+++ src/main/java/com/cohort10/example/Hello.java
@@ -1,21 +1,80 @@
 package com.cohort10.example;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Hello extends GenericServlet {
+public class Hello extends HttpServlet {
 
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String action = req.getParameter("action");
 
         PrintWriter wr = res.getWriter();
         if (action != null && action.equalsIgnoreCase("register"))
-            wr.print(this.register());
+            wr.print(this.register(null));
         else if (action != null && action.equalsIgnoreCase("login"))
-            wr.print(this.login());
+            wr.print(this.login(null));
         else
             wr.print(this.home());
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter wr = res.getWriter();
+
+        String action = req.getParameter("action");
+        String password = req.getParameter("password");
+        String confirmPassword = req.getParameter("confirmPassword");
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+
+        boolean register = action != null && action.equalsIgnoreCase("register");
+        boolean login = action != null && action.equalsIgnoreCase("login");
+
+        String actionError = "";
+        if (register) {
+            System.out.println("Email: " + email);
+            System.out.println("Password: " + password);
+            System.out.println("Confirm Password: " + confirmPassword);
+
+            if (email == null || email.equalsIgnoreCase(""))
+                actionError = "Email is required<br/>";
+
+            if (password == null || password.equalsIgnoreCase(""))
+                actionError += "Password is required<br/>";
+
+            if (confirmPassword == null || confirmPassword.equalsIgnoreCase(""))
+                actionError += "Confirm password is required<br/>";
+
+            if (password != null && confirmPassword != null && !password.equals(confirmPassword))
+                actionError += "Password & confirm password do not match<br/>";
+
+            if (actionError.equals(""))
+                wr.print(this.home());
+            else
+                wr.print(this.register(actionError));
+
+        } else if (login) {
+            System.out.println("username: " + username);
+            System.out.println("Password: " + password);
+
+            if (username == null || username.equalsIgnoreCase(""))
+                actionError = "Username is required<br/>";
+
+            if (password == null || password.equalsIgnoreCase(""))
+                actionError += "Password is required<br/>";
+
+            if (password != null && !password.equals("12345"))
+                actionError += "Invalid password<br/>";
+
+            if (actionError.equals(""))
+                wr.print(this.loggedIn());
+            else
+                wr.print(this.login(actionError));
+
+        }
     }
 
     public String home(){
@@ -31,37 +90,55 @@ public class Hello extends GenericServlet {
             + "</html>";
     }
 
-    public String register(){
+    public String register(String actionError){
         return "<!DOCTYPE html>"
             + "<html> "
                 + "<head> "
                     + "<h1> Registration</h1>"
                 + "</head>"
                 + "<body>"
-                    + "<form>"
+                    + "<form action=\"./welcome\" method=\"post\">"
                         + "<table> "
-                            + "<tr> <td> Email: </td> <td> <input type=”text” email=”email”> </td> </tr> "
-                            + "<tr> <td> Password: </td> <td> <input type=”Password” name=”password”> </td> </tr> "
-                            + "<tr> <td> Confirm Password: </td> <td> <input type=”Password” name=”confirmPassword”> </td> </tr> "
+                            + "<tr> <td> Action: </td> <td> <input type=\"text\" name=\"action\" value=\"register\"> </td> </tr> "
+                            + "<tr> <td> Email: </td> <td> <input type=\"text\" name=\"email\"> </td> </tr> "
+                            + "<tr> <td> Password: </td> <td> <input type=\"Password\" name=\"password\"> </td> </tr> "
+                            + "<tr> <td> Confirm Password: </td> <td> <input type=\"Password\" name=\"confirmPassword\"> </td> </tr> "
+                            + "<tr> <td> <input type=\"submit\" value=\"Submit\"></tr> "
                         + "</table>"
                     + "</form>"
+                    + "<span style=\"color:red\">" + (actionError != null? actionError : "") + "</span>"
                 + "</body>"
             + "</html>";
     }
 
-    public String login(){
+    public String login(String actionError){
         return "<!DOCTYPE html>"
             + "<html> "
                 + "<head> "
                     + "<h1> Login</h1>"
                 + "</head>"
                 + "<body>"
-                    + "<form>"
+                    + "<form action=\"./welcome\" method=\"post\">"
                         + "<table> "
-                            + "<tr> <td> Email/Username: </td> <td> <input type=”text” email=”username”> </td> </tr> "
-                            + "<tr> <td> Password: </td> <td> <input type=”Password” name=”password”> </td> </tr> "
+                            + "<tr> <td> Action: </td> <td> <input type=\"text\" name=\"action\" value=\"login\"> </td> </tr> "
+                            + "<tr> <td> Email/Username: </td> <td> <input type=\"text\" name=\"username\"> </td> </tr> "
+                            + "<tr> <td> Password: </td> <td> <input type=\"Password\" name=\"password\"> </td> </tr> "
+                            + "<tr> <td> <input type=\"submit\" value=\"Submit\"></tr> "
                         + "</table>"
                     + "</form>"
+                    + "<span style=\"color:red\">" + (actionError != null? actionError : "") + "</span>"
+                + "</body>"
+            + "</html>";
+    }
+
+    public String loggedIn(){
+        return "<!DOCTYPE html>"
+            + "<html> "
+                + "<head> "
+                    + "<h1> Login</h1>"
+                + "</head>"
+                + "<body>"
+                    + "<span style=\"color:green;font-size: 24px;font-weight:bold\">Logged In</span>"
                 + "</body>"
             + "</html>";
     }
